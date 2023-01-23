a020e7025b7572c64ebbd7aa5ad6d052056635d4
Ryan Ellis <ryan.ellis@gatech.edu>
2023-01-23 13:29:18 -0500

    removed bad file

---
+++ homework2/homework2.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 23 13:17:57 2023
+
+@author: rellis
+"""
+
+import pandas as pd
+data = pd.read_csv("C:\\Users\\rellis\\Dropbox (GaTech)\\PHD_AND_COURSES\\SPRING 2023\\ENVIRO 2\\phdee-2023-RE\\homework2\\kwh.csv")
+data.head()
